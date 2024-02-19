5a85b470d8cb92128f8b6de084983e9fb455e682
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-02-19 15:26:03 +0100

    [add] Password Color Change

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const firstName = prompt('Inserisci il tuo nome');
 const lastName = prompt('Inserisci il tuo cognome');
-const favoriteColor = prompt('Inserisci il tuo colore preferito');
+const favoriteColor = prompt('Inserisci il tuo colore preferito, se lo scrivi in inglese è meglio!');
 const favoriteNumber = prompt('Inserisci il tuo numero preferito')
 const password = firstName + lastName + favoriteColor + favoriteNumber;
 
 console.log(password);
 
 document.getElementById('pwd-generated').innerHTML = 
-`Questa è la tua password: <strong>${password}</strong>`;
+`Questa è la tua password: <strong style="color:${favoriteColor}">${password}</strong>`;
